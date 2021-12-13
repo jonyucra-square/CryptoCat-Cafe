bf507daa80aea9376745aaa857491cb5026b390f
Jonatan Yucra Rodriguez <jonatany@squareup.com>
2021-12-13 17:20:18 -0500

    Support sectioned responses, post cats to team members on a timer.

--- app.js
+++ app.js
@@ -10,25 +10,72 @@ app.get('/', (req, res) => {
 })
 
 app.post('/randomCat', (req, res) => {
-  const maxId = 2008190;
-  const catId = Math.floor(Math.random() * maxId);
-  const imgSource = `https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/${catId}.png`;
-  const response = {
+
+  const data = 	{
     "response_type": "in_channel",
-    "text": imgSource
-  }
-  res.send(response)
+    "blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "Hi, have a cat :cat:"
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": getRandomCatImageSource()
+        }
+      }
+    ]
+  };
+
+  res.send(data)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+const teamMemberIDs = [
+  "U02QG12AG0M", // Kevin
+  "U02Q4A4866T", // Taylor
+  "U02Q4A4866T", // Chris
+  "U02QM7H4BFW", // Kashyap
+  "U02QC8DHCP8", // Sophie
+  "U02QM7FPWBW", // Sam
+  "U02QM7C867N", // Jonatan
+  "U02QD43GPN2", // Daniel
+];
+
+function getRandomCatImageSource() {
+  const maxId = 2008190;
+  const catId = Math.floor(Math.random() * maxId);
+  const imgSource = `https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/${catId}.png`;
+  return imgSource;
+}
+
 function postCatMessage() {
-  const data = {
-    "text": "Hello Cat World"
-  }
-  axios.post('https://hooks.slack.com/services/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', data)
+  const data = 	{
+    "blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "Hi <@U02QM7C867N>, have a cat :cat:"
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": getRandomCatImageSource()
+        }
+      }
+    ]
+  };
+  axios.post('https://hooks.slack.com/services/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', data)
     .then((res) => {
       console.log("POST SENT: ", res);
     })
@@ -37,7 +84,37 @@ function postCatMessage() {
     });
 }
 
-cron.schedule('*/45 * * * * *', function() {
+function postCatMessageToGeneral() {
+  const randomUserId = teamMemberIDs[Math.floor(Math.random()*teamMemberIDs.length)];
+  const data = 	{"blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hi <@${randomUserId}>, have a cat :cat:`
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": getRandomCatImageSource()
+        }
+      }
+    ]
+  };
+  console.log("Random ID: ", randomUserId);
+  axios.post('https://hooks.slack.com/services/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', data)
+    .then((res) => {
+      console.log("POST SENT: ", res);
+    })
+    .catch((err) => {
+      console.log("ERROR: ", err);
+    });
+}
+
+cron.schedule('*/60 * * * * *', function() {
   console.log("Running cron job!");
-  postCatMessage();
+  //postCatMessageToGeneral();
+  //postCatMessage();
 });
